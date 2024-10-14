d90e4d70428c8214811ed66750d773440b2c4a20
victordiogouf <victor.diogo@estudante.ufjf.br>
2024-10-14 19:44:49 -0300

    desempacotamento, interpretador

--- 51-desempacotamento.py
+++ 51-desempacotamento.py
@@ -1,3 +1,5 @@
 name1, *_ = ['João', 'Maria', 'José'] # Just getting the first element
 
 _, name1, _ = ['João', 'Maria', 'José'] # Just getting the second element
+
+name1, *_, last = ['João', 'Maria', 'José'] # Getting the first and last elements
